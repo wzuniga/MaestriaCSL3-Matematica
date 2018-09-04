8a48daa76c59e780cacbdfd0bbe072d9754c3bb3
LuisErnesto <agoraphobiacroz@gmail.com>
2018-09-04 18:45:17 -0500

    remove line

--- Seidel.m
+++ Seidel.m
@@ -1,14 +1,13 @@
 % procedimiento seidel
-function [Uij] = Seidel(Ax, Ay, Nx, Ny, Itmax, Uij)
+function [U] = Seidel(Ax, Ay, Nx, Ny, Itmax, U)
 %i, j, k;
-
 for k = 1:Itmax
 	for j = 1:Nx-1
 		y = Ax + j*h;
 		for i = 1:Nx-1
 			x = Ax + i*h;
 			v = U(i+1,j) + U(i-1,j) + U(i,j-1);
-			%U(i,j) = (v - h^2*)/(4-h^2 f(x, y));
+			U(i,j) = (v - pow2(h)*g(x,y))/(4-pow2(h)*f(x, y));
 		end
 	end
 end
